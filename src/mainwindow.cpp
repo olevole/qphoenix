072cecbda291e265f0a898809296bfdc40e0721b
flareguner <flareguner@gmail.com>
2012-11-16 23:06:49 +0400

    Minor sources change.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "historymanager/historymanager.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    HistoryManager m; m.exec();
 }
 
 MainWindow::~MainWindow()
