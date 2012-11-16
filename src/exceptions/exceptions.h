7b17ba0ba23c770d82c0acd7a789b6bfeeebd264
flareguner <flareguner@gmail.com>
2012-11-16 23:05:29 +0400

    Minor changes, documents editions, etc

---
+++ src/exceptions/exceptions.h
@@ -0,0 +1,22 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <QDialog>
+
+namespace Ui {
+class Exceptions;
+}
+
+class Exceptions : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit Exceptions(QWidget *parent = 0);
+    ~Exceptions();
+    
+private:
+    Ui::Exceptions *ui;
+};
+
+#endif // EXCEPTIONS_H
