a5c4ae041d640303e6536b831d07ad51d717ffcf
flareguner <flareguner@gmail.com>
2012-11-20 09:06:05 +0400

    Integration started

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include "settings.h"
 #include "exceptions.h"
 
+#include "fancytabwidget.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 //    MainWindow w;
 //    w.show();
 
-
+    Core::Internal::FancyTabWidget w;
     Settings s;
     return s.exec();
 
