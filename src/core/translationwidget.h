4f35a5113f940d52d59758d1ae881829f10b79b2
flareguner <flareguner@gmail.com>
2012-12-15 15:32:57 +0400

    Tt

---
+++ src/core/translationwidget.h
@@ -0,0 +1,54 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *    Copyright: QPhoenix team
+ *    E-Mail: development@qphoenix.org
+ *    Years: 2012-2013
+ */
+
+#ifndef TRANSLATIONWIDGET_H
+#define TRANSLATIONWIDGET_H
+
+#include <QWidget>
+#include "pluginapi.h"
+
+class QTextEdit;
+class QTextBrowser;
+class QToolButton;
+class QPushButton;
+class QComboBox;
+
+class TranslationWidget : public QWidget, Api::TranslatorWidgetAPI
+{
+    Q_OBJECT
+    Q_INTERFACES(Api::TranslatorWidgetAPI)
+public:
+    explicit TranslationWidget(QWidget *parent = 0);
+
+    virtual QComboBox   *srcComboBox(){}
+    virtual QComboBox   *resComboBox(){}
+    virtual QTextEdit   *srcText(){}
+    virtual QTextBrowser*resTExt(){}
+    virtual QPushButton *translateButton(){}
+    virtual QToolButton *swapButton(){}
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // TRANSLATIONWIDGET_H
